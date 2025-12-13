9f5586d38cf6177aefc5393517e6903e433986ff
mohankharat27 <kharatmohan2244@gmail.com>
2025-12-13 16:26:27 +0530

    Creating an element

--- script.js
+++ script.js
@@ -1,95 +1,25 @@
-// ===== Buld =====
-
-let btn = document.querySelector("button");
-let bulb = document.querySelector("#bulb");
-
-let flag = 0;
-btn.addEventListener("click",function(){
-    if(flag == 0){
-        bulb.style.backgroundColor = "Green";
-        btn.innerHTML = "OFF";
-        flag = 1;
-    }else{
-        bulb.style.backgroundColor = "transparent";
-        btn.innerHTML = "ON"
-        flag = 0;
-    }
-})
-
-// Change the text of a <p> tag when a button is clicked.
-
-let para = document.querySelector('p');
-let btn1 = document.querySelector('button');
-
-btn.addEventListener("click",function(){
-    para.innerHTML = "I am Software Engineer!";
-})
-
-// Change the background color of the page when a button is clicked.
-
-let page = document.body;
-
-page.addEventListener("click", function(){
-    page.style.backgroundColor = "Crimson";
-})
-
-
-// Select an element using:
-
-getElementById
-
-let a = document.getElementById("first");
-
-a.addEventListener("click",function(){
-    a.style.backgroundColor = "Red";
-})
-
-getElementsByClassName
-
-let b = document.getElementsByClassName("second");
-
-b.addEventListener("click",function(){
-    arguments.style.backgroundColor = "yellow"; 
-})
-
-// querySelector
-
-// querySelectorAll
-// and print them in console.
-
-let all = document.querySelectorAll("h1");
-console.log(all);
-
-
-// When user clicks a button → hide a div (display: none).
-// When user clicks another button → show the div again.
-
-let box = document.querySelector("#box");
+let main = document.querySelector("main");
 let button = document.querySelector("button");
-let button2 = document.querySelector("#btn2");
 
-let flag1 = 0;
+button.addEventListener("click",function() {
+    let div = document.createElement("div");
+    
+    let height = Math.random()*100;
+    let width = Math.random()*100;
 
-button.addEventListener("click",function(){
-    box.style.display = "none";
-    flag = 1;
+    div.style.height = height +"px"
+    div.style.width = width +"px"
+
+    let c1 = Math.random()*256;
+    let c2 = Math.random()*256;
+    let c3 = Math.random()*256;
+    div.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+    
+    let top = Math.random()*100;
+    let left = Math.random()*100;
+
+    div.style.top = top + "%";
+    div.style.left = left + "%";
+    div.style.position = "absolute"
+    main.appendChild(div);
 })
-
-button2.addEventListener("click",function(){
-    box.style.display = "block";
-    flag = 0;
-})
-
-
-// Change image source (src) on button click.
-
-let img = document.getElementById("img");
-let btn2 = document.getElementById("toggleBtn");
-
-btn.addEventListener("click", function () {
-    if (img.src.includes("image1.png")) {
-        img.src = "image2.png";
-    } else {
-        img.src = "image1.png";
-    }
-});
