3ae6736e30c5b77ac6a62055e7be8e65364e356f
mohankharat27 <kharatmohan2244@gmail.com>
2025-12-13 11:19:58 +0530

    IPL Winner Team

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-// let box = document.querySelector("#box");
-// let button = document.querySelector("button");
-
-// let c1 = Math.floor(Math.random()*256);
-// let c2 = Math.floor(Math.random()*256);
-// let c3 = Math.floor(Math.random()*256);
-
-// button.addEventListener("click",function(){
-//     box.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
-// }) 
-
-// 2.
-
-let arr = ['mohan','raj','akash','aditya','vaishnavi','tanuja','omkar','rutuja'];
-
-let a = Math.floor(Math.random()*arr.length);
-
-console.log(arr[a]);
+let box = document.querySelector("div");
+let button = document.querySelector("button")
+let main = document.querySelector("main");
+let title = document.querySelector("h1");
 
 
+let arr = [
+    {
+        team : "RCB",
+        primary : "Red",
+        secondary : "pink",
+        captain : "Rajat Patidar"
+    },
+    {
+        team : "MI",
+        primary : "blue",
+        secondary : "gold",
+        captain : "Hardik Pandya"
+    },
+    {
+        team : "GT",
+        primary : "blue",
+        secondary : "yellow",
+        captain : "Shubhman Gill"
+    },
+    {
+        team : "SRH",
+        primary : "orange",
+        secondary : "red",
+        captain : "Pat Cummins"
+    },
+    {
+        team : "PBKS",
+        primary : "crimson",
+        secondary : "yellow",
+        captain : "Shreyas Iyer"
+    }
+]
+
+
+button.addEventListener("click",function(){
+    let a = Math.floor(Math.random()*arr.length);
+    box.style.backgroundColor = arr[a].primary;
+    main.style.backgroundColor = arr[a].secondary;
+    title.innerText = arr[a].team;
+
+})
