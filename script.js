bf9d18fbb337bd70c42446258e785e867b4df5f4
mohankharat27 <kharatmohan2244@gmail.com>
2025-12-13 19:11:59 +0530

    Download scroll bar

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-let main = document.querySelector("main");
-let button = document.querySelector("button");
+let btn = document.querySelector("button");
+let h2 = document.querySelector("h2");
+let inner = document.querySelector(".inner")
 
-let arr = ["Mohan is Great!", "Js is Love", "I am from Pune", "Learning Full Stack","Software Engineer", "I am consistent", "I am Unique", "I am brave", "I am Good", "I am Learner", "Software Developer", "HTML", "CSS", "JavaScript", "MongoDB", "Express.js", "React", "Life is Good!"]
+let grow = 0;
+let int;
 
-button.addEventListener("click",function() {
-    let h1 = document.createElement("h1");
+btn.addEventListener("click",function() {
+    int = setInterval(() => {
+        grow++;
+        h2.innerHTML = grow + "%";
+        inner.style.width = grow + "%"
 
-    let sen = Math.floor(Math.random() * arr.length + 1);
-    h1.innerText = arr[sen];
+        if(grow >= 100){
+            clearInterval(int);
+        }
+    }, 50);
 
-    let top = Math.random()*100;
-    let left = Math.random()*100;
+    setTimeout(function() {
+    clearInterval(int)
+    },5000)
+})   
 
-    h1.style.top = top + "%";
-    h1.style.left = left + "%";
-    h1.style.position = "absolute"
-
-    let c1 = Math.random()*256;
-    let c2 = Math.random()*256;
-    let c3 = Math.random()*256;
-
-    h1.style.color = `rgb(${c1},${c2},${c3})`
-    main.appendChild(h1);
-})
