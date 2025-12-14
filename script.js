ce3e0a900ab344a0b329296dc0789d32bbb93b5b
mohankharat27 <kharatmohan2244@gmail.com>
2025-12-14 18:56:25 +0530

    cards

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
+let arr = [
+    {
+        fullName: "Aarav Mehta",
+        role: "Data Scientist",
+        caption: "Works with data analysis, machine learning models, and predictive analytics.",
+        img: "https://images.unsplash.com/photo-1552058544-f2b08422138a?q=80&w=687&auto=format&fit=crop"
+    },
+    {
+        fullName: "Pooja Kulkarni",
+        role: "Cloud Engineer",
+        caption: "Manages cloud infrastructure, deployment pipelines, and system scalability.",
+        img: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=687&auto=format&fit=crop"
+    },
+    {
+        fullName: "Imran Khan",
+        role: "Cyber Security Analyst",
+        caption: "Monitors threats, performs penetration testing, and secures applications.",
+        img: "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?q=80&w=687&auto=format&fit=crop"
+    },
+    {
+        fullName: "Neha Joshi",
+        role: "Product Manager",
+        caption: "Bridges business and technology to deliver impactful digital products.",
+        img: "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?q=80&w=687&auto=format&fit=crop"
+    }
+];
 
-var img = document.querySelector('img')
-var love = document.querySelector('#love')
 
-img.addEventListener('dblclick', function () {
+let sum = '';
 
-    love.style.opacity = 1
-    love.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)'
-
-    setTimeout(function () {
-        love.style.transform = 'translate(-50%,-300%) scale(1) rotate(60deg)'
-    }, 800)
-    setTimeout(function () {
-        love.style.opacity = 0
-    }, 1000)
-    setTimeout(function () {
-        love.style.transform = 'translate(-50%,-50%) scale(0) rotate(-60deg)'
-    }, 1200)
-
+arr.forEach(function(elem){
+    sum = sum + `<div class="card">
+            <img src="${elem.img}" alt="">
+            <h3>${elem.fullName}</h3>
+            <h4>${elem.role}</h4>
+            <p>${elem.caption}</p>
+        </div>`;
 })
+
+let main = document.querySelector('main');
+
+main.innerHTML = sum;
